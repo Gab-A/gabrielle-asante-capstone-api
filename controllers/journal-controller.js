89a5f98f4df11a8bf2869b6b8a507d957e6f5dac
Gabrielle Asante <asante.gabrielle@gmail.com>
2023-11-22 00:00:37 +0000

    Refactor put and delete journal endpoints

--- controllers/journal-controller.js
+++ controllers/journal-controller.js
@@ -53,17 +53,11 @@ const createJournal = async (req, res) => {
   const { content, title, users_id, mood } = req.body;
 
   const newJournal = {
-    users_id: users_id,
+    // users_id: users_id,
     content: content,
     title: title,
-    mood: mood,
+    // mood: mood,
   };
-
-  const checkUserExistence = await knex("users").where("id", users_id).first();
-
-  if (!checkUserExistence) {
-    return res.status(404).json({ error: "User not found" });
-  }
 
   try {
     const result = await knex("journal").insert(newJournal);
@@ -71,9 +65,10 @@ const createJournal = async (req, res) => {
       .where({ id: result[0] })
       .first();
     res.status(201).send(createdJournal);
+    console.log(createJournal);
   } catch (error) {
     res.status(500).json({
-      message: `Because of this ${error}, this note cannot be created.`,
+      message: `Because of this ${error}, this journal cannot be created.`,
     });
   }
 };
@@ -81,30 +76,20 @@ const createJournal = async (req, res) => {
 // Update journal:
 
 const updateJournal = async (req, res) => {
-  // const checkExists = await knex("journal")
-  //   .where({ id: req.params.id })
-  //   .first();
-
-  // if (!checkExists) {
-  //   return res
-  //     .status(404)
-  //     .send({ message: `Journal with id ${req.params.id} could not be found` });
-  // }
-
-  // const requestedUser = req.body.users_id;
-  // const checkUserExists = await knex("users")
-  //   .where({ id: requestedUser })
-  //   .first();
-  // if (!checkUserExists) {
-  //   return res
-  //     .status(404)
-  //     .send({ message: `User with id ${requestedUser} does not exist.` });
-  // }
   try {
-    const editJournal = await knex("journal")
+    const outcome = await knex("journal")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (outcome === 0) {
+      return res.status(404).send({
+        message: `Journal with ID ${req.params.id} not found`,
+      });
+    }
+    const updatedJournal = await knex("journal")
       .where({ id: req.params.id })
       .first();
-    return res.status(200).send(editJournal);
+    return res.status(200).send(updatedJournal);
   } catch (error) {
     res.status(500).json({
       message: `Because of this ${error}, this journal entry cannot be updated.`,
@@ -116,25 +101,17 @@ const updateJournal = async (req, res) => {
 
 const deleteJournal = async (req, res) => {
   try {
-    // const journalItem = await knex("journal")
-    //   .where({ id: req.params.id })
-    //   .first();
+    const outcome = await knex("journal").where({ id: req.params.id }).del();
 
-    // // if (!journalItem) {
-    // //   return res.status(404).send({
-    // //     message: `Journal with ID ${req.params.id} not found`,
-    // //   });
-    // // }
+    if (outcome === 0) {
+      return res
+        .status(404)
+        .json({ message: `Journal with ID ${req.params.id} not found` });
+    }
 
-    await knex("journal").where({ id: req.params.id }).del();
-
-    res.status(204).send({
-      message: `Journal with ID ${req.params.id} has been deleted successfully`,
-    });
+    res.sendStatus(204);
   } catch (error) {
-    console.error(error);
-
-    res.status(500).send({
+    res.status(500).json({
       message: `Unable to delete journal with ID ${req.params.id}`,
     });
   }
