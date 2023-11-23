64462d2cf6f5683c421b917ed327ff5aa1fc70d3
Gabrielle Asante <asante.gabrielle@gmail.com>
2023-11-23 17:34:14 +0000

    Refactor get journal by id function

--- controllers/journal-controller.js
+++ controllers/journal-controller.js
@@ -5,7 +5,7 @@ const knex = require("knex")(require("../knexfile"));
 const getAllJournals = async (_req, res) => {
   try {
     const response = await knex("journal")
-      .join("users", "users.id", "journal.users_id")
+      // .join("users", "users.id", "journal.users_id")
       .select(
         "journal.id",
         "journal.users_id",
@@ -16,6 +16,7 @@ const getAllJournals = async (_req, res) => {
         "journal.updated_at"
       );
     res.status(200).json(response);
+    console.log(response);
   } catch (error) {
     console.error(error);
     res.status(400).send(`Error retrieving journals: ${error}`);
@@ -27,7 +28,7 @@ const getAllJournals = async (_req, res) => {
 const getJournalById = async (req, res) => {
   try {
     const journal = await knex("journal")
-      .join("users", "users.id", "journal.users_id")
+      // .join("users", "users.id", "journal.users_id")
       .select(
         "journal.id",
         "journal.users_id",
@@ -51,22 +52,25 @@ const getJournalById = async (req, res) => {
 // Create journal:
 const createJournal = async (req, res) => {
   const { content, title, users_id, mood } = req.body;
+  console.log(content, title);
 
   const newJournal = {
     // users_id: users_id,
-    content: content,
-    title: title,
-    // mood: mood,
+    content,
+    title,
+    mood,
   };
 
   try {
     const result = await knex("journal").insert(newJournal);
+    // console.log(result);
     const createdJournal = await knex("journal")
       .where({ id: result[0] })
       .first();
     res.status(201).send(createdJournal);
     console.log(createJournal);
   } catch (error) {
+    console.error(error);
     res.status(500).json({
       message: `Because of this ${error}, this journal cannot be created.`,
     });
